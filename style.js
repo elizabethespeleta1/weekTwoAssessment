7ba2903bec9877b7def914dbb30146bec91924ee
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-04-29 10:56:12 -0700

    finished le test

---
+++ style.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+		alert("hey you clicked meh");
+	});
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+		$('#iwao').hide();
+	});
+});
